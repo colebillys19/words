40afaad1be61712bf5b1559beb71de60142df046
Billy Cole <cole.billys@gmail.com>
2019-11-22 14:03:22 -0800

    clean up Landing & NotFoundPage, remove markdown

---
+++ app/containers/Landing/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Landing() {
+  return <div>Landing</div>;
+}
--- app/containers/NotFoundPage/index.tsx
+++ app/containers/NotFoundPage/index.tsx
@@ -1,20 +1,5 @@
-/**
- * NotFoundPage
- *
- * This is the page we show when the user visits a url that doesn't have a route
- */
-
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
 
 export default function NotFound() {
-  return (
-    <article>
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    </article>
-  );
+  return <div>Not Found</div>;
 }
